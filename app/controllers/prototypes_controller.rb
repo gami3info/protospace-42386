d2b8ebb589245d6f8c16d7994da463257912ae50
gami3info <mugennotsubasa374@gmail.com>
2025-07-09 16:11:43 +0900

    他ユーザーで編集ができてしまう事象の修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,9 +1,7 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :destroy]
-  before_action :move_to_index, except: [:index,:show,:edit]
-  def index
-      @prototypes = Prototype.all
-  end
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_prototype, only: [:show, :edit, :update, :destroy]
+  before_action :authorize_user, only: [:edit, :update, :destroy]
 
   def new
     @prototype = Prototype.new
@@ -17,39 +15,40 @@ class PrototypesController < ApplicationController
       render :new, status: :unprocessable_entity
     end
   end
+  def index
+      @prototypes = Prototype.all
+  end
   def show
-    @prototype = Prototype.find(params[:id])
     @comment = Comment.new
     @comments = @prototype.comments.includes(:user)
   end
 
-  def edit
-    @prototype = Prototype.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @prototype = Prototype.find(params[:id])
     if @prototype.update(prototype_params)
-       redirect_to prototype_path
+       redirect_to prototype_path(@prototype)
     else
       render :edit, status: :unprocessable_entity
     end
   end
 
   def destroy
-    @prototype = Prototype.find(params[:id])
     @prototype.destroy
     redirect_to root_path
   end
 
   private
-  def prototype_params
-    params.require(:prototype).permit(:title, :catch_copy, :concept,:image).merge(user_id: current_user.id)
+
+  def authorize_user
+    redirect_to root_path unless @prototype.user == current_user
   end
 
-    def move_to_index
-    unless user_signed_in?
-      redirect_to action: :index
-    end
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+
+  def set_prototype
+    @prototype = Prototype.find(params[:id])
   end
 end
